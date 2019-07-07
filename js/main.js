421b8db2ba7850508c23617a83d92ff4959db4a7
Jack Cherng <jfcherng@gmail.com>
2019-07-07 09:06:20 +0800

    Only print NTHU slogan in non-framed page
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- js/main.js
+++ js/main.js
@@ -4,5 +4,11 @@ const debug = false;
 
 const $ = global.$;
 const rcmail = global.rcmail;
+const rcube_parse_query = global.rcube_parse_query;
 
-funcs.printNthuSloganToConsole();
+let comm_path = rcube_parse_query(rcmail.env.comm_path);
+let is_framed = Boolean(parseInt(typeof comm_path._framed !== 'undefined' ? comm_path._framed : 0));
+
+if (!is_framed) {
+  funcs.printNthuSloganToConsole();
+}
