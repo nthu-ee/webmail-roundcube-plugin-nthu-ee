2c4b2ebf78797228ca2cdc07bfeb578edde90b92
Jack Cherng <jfcherng@gmail.com>
2020-04-26 02:06:20 +0800

    fix: use production NTHU EE logo URL
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@ const printNthuSloganToConsole = () => console.log(
     font-family: Arial;
     font-size: 60px;
     background:
-      url(https://webmail.ee.nthu.edu.tw/new/skins/elastic/images/logo-phone.png)
+      url(https://webmail.ee.nthu.edu.tw/skins/elastic/images/logo-phone.png)
       left center no-repeat;
     background-size: 80%;
   `
