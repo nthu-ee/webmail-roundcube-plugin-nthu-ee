fa6bc381fe1cbc9be0dee02365a9d57c2e1c7140
Jack Cherng <jfcherng@gmail.com>
2020-04-26 02:01:06 +0800

    refactor: extract common methods between plugins
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- nthu_ee.php
+++ nthu_ee.php
@@ -2,59 +2,62 @@
 
 declare(strict_types=1);
 
+include __DIR__ . '/lib/vendor/autoload.php';
+
+use Jfcherng\Roundcube\Plugin\NthuEe\RoundcubePluginTrait;
+
 final class nthu_ee extends rcube_plugin
 {
+    use RoundcubePluginTrait;
+
     /**
      * {@inheritdoc}
      */
     public $task = '';
 
     /**
-     * The loaded configuration.
+     * Plugin actions and handlers.
      *
-     * @var rcube_config
+     * @var array<string,string>
      */
-    private $config;
+    public $actions = [];
+
+    /**
+     * The plugin configuration.
+     *
+     * @var array
+     */
+    private $config = [];
+
+    /**
+     * The plugin user preferences.
+     *
+     * @var array
+     */
+    private $prefs = [];
 
     /**
      * {@inheritdoc}
      */
     public function init(): void
     {
-        $this->load_plugin_config();
+        $this->loadPluginConfigurations();
+        $this->loadPluginPreferences();
+        $this->registerPluginActions();
 
-        // the current skin name like from `$this->config->get('skin')` is not much
-        // helpful for extended skins, we try to get it's base skin name directly
-        $base_skin = $this->get_base_skin_name();
-
-        $this->add_texts('locales', true);
-
-        $this
-            ->add_plugin_assets($base_skin)
-            ->add_plugin_buttons($base_skin);
-    }
-
-    /**
-     * Add plugin assets.
-     *
-     * @param string $skin the skin name
-     */
-    private function add_plugin_assets(string $skin): self
-    {
-        $this->include_stylesheet("skins/{$skin}/main.css");
+        $this->add_texts('localization/', false);
+        $this->include_stylesheet($this->local_skin_path() . '/main.css');
         $this->include_script('js/main.min.js');
 
-        return $this;
+        $this->addPluginButtons();
     }
 
     /**
      * Add plugin buttons.
-     *
-     * @param string $skin the skin name
      */
-    private function add_plugin_buttons(string $skin): self
+    private function addPluginButtons(): void
     {
-        $this->add_plugin_buttons_loginfooter([
+        $this->add_buttons_loginfooter([
             [
                 'label' => "{$this->ID}.old_webmail",
                 'title' => "{$this->ID}.old_webmail_never_receive_new_mail",
@@ -71,154 +74,15 @@ final class nthu_ee extends rcube_plugin
                 'href' => 'http://web.ee.nthu.edu.tw',
                 'target' => '_blank',
             ],
-        ], $skin);
+        ]);
 
-        $this->add_plugin_buttons_taskbar([
+        $this->add_buttons_taskbar([
             [
                 'label' => "{$this->ID}.manual",
                 'title' => "{$this->ID}.open_manual",
                 'href' => 'skins/.manual/',
                 'target' => '_blank',
             ],
-        ], $skin);
-
-        return $this;
-    }
-
-    /**
-     * Add the plugin buttons to loginfooter.
-     *
-     * @param array  $btns the buttons
-     * @param string $skin the skin name
-     */
-    private function add_plugin_buttons_loginfooter(array $btns, string $skin): self
-    {
-        $btns = \array_map(function (array $btn) use ($skin): array {
-            $btn['type'] = 'link';
-            $btn['classArray'] = $this->class_string_to_array($btn['class'] ?? '');
-            $btn['innerclassArray'] = $this->class_string_to_array($btn['innerclass'] ?? '');
-            $btn['badgeType'] = $btn['badgeType'] ?? 'secondary';
-
-            // should always has 'support-link' class
-            $btn['classArray'][] = 'support-link';
-
-            if ($skin === 'elastic') {
-                $btn['classArray'][] = 'badge';
-                $btn['classArray'][] = "badge-{$btn['badgeType']}";
-                $btn['data-toggle'] = $btn['data-toggle'] ?? 'tooltip';
-            }
-
-            $btn['class'] = $this->class_array_to_string($btn['classArray']);
-            $btn['innerclass'] = $this->class_array_to_string($btn['innerclassArray']);
-
-            return $btn;
-        }, $btns);
-
-        foreach ($btns as $btn) {
-            $this->add_button($btn, 'loginfooter');
-        }
-
-        return $this;
-    }
-
-    /**
-     * Add the plugin buttons to taskbar.
-     *
-     * @param array  $btns the buttons
-     * @param string $skin the skin name
-     */
-    private function add_plugin_buttons_taskbar(array $btns, string $skin): self
-    {
-        $btns = \array_map(function (array $btn) use ($skin): array {
-            $btn['type'] = 'link';
-            $btn['classArray'] = $this->class_string_to_array($btn['class'] ?? '');
-            $btn['innerclassArray'] = $this->class_string_to_array($btn['innerclass'] ?? '');
-
-            switch ($skin) {
-                case 'classic':
-                    $btn['classArray'][] = 'button-nthu-ee';
-                    break;
-                case 'elastic':
-                    $btn['classArray'][] = 'nthu-ee';
-                    $btn['classArray'][] = 'manual';
-                    $btn['innerclassArray'][] = 'inner';
-                    break;
-                case 'larry':
-                    $btn['classArray'][] = 'button-nthu-ee';
-                    $btn['innerclassArray'][] = 'button-inner';
-                    break;
-                default:
-                    break;
-            }
-
-            $btn['class'] = $this->class_array_to_string($btn['classArray']);
-            $btn['innerclass'] = $this->class_array_to_string($btn['innerclassArray']);
-
-            return $btn;
-        }, $btns);
-
-        foreach ($btns as $btn) {
-            $this->add_button($btn, 'taskbar');
-        }
-
-        return $this;
-    }
-
-    /**
-     * Convert the array of classes to HTML class attribute.
-     *
-     * @param array $class_array the array of classes
-     */
-    private function class_array_to_string(array $class_array): string
-    {
-        return \implode(' ', $this->class_string_to_array(\implode(' ', $class_array)));
-    }
-
-    /**
-     * Convert the HTML class attribute to an array of classes.
-     *
-     * @param string $class_string the HTML class attribute
-     */
-    private function class_string_to_array(string $class_string): array
-    {
-        return \preg_match_all('/[^\s]++/uS', $class_string, $matches)
-            ? \array_keys(\array_count_values($matches[0])) // fast array unique
-            : [];
-    }
-
-    /**
-     * Load plugin configuration.
-     */
-    private function load_plugin_config(): void
-    {
-        $rcmail = rcmail::get_instance();
-
-        $this->load_config('config.inc.php.dist');
-        $this->load_config('config.inc.php');
-
-        $this->config = $rcmail->config;
-    }
-
-    /**
-     * Get the lowercase base skin name for the current skin.
-     *
-     * @return string the base skin name
-     */
-    private function get_base_skin_name(): string
-    {
-        static $base_skins = ['classic', 'larry', 'elastic'];
-
-        $rcube = rcube::get_instance();
-
-        // information about current skin and extended skins (if any)
-        $skins = (array) $rcube->output->skins;
-
-        foreach ($base_skins as $base_skin) {
-            if (isset($skins[$base_skin])) {
-                return $base_skin;
-            }
-        }
-
-        return $skins[0] ?? '';
+        ]);
     }
 }
---
+++ src/RoundcubePluginTrait.php
@@ -0,0 +1,274 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jfcherng\Roundcube\Plugin\NthuEe;
+
+use rcmail;
+
+trait RoundcubePluginTrait
+{
+    /**
+     * Append a button to a certain container.
+     *
+     * @param array  $p         Hash array with named parameters (as used in skin templates)
+     * @param string $container Container name where the buttons should be added to
+     *
+     * @see rcube_remplate::button()
+     */
+    abstract public function add_button(array $p, string $container);
+
+    /**
+     * Register a handler for a specific client-request action.
+     *
+     * The callback will be executed upon a request like /?_task=mail&_action=plugin.myaction
+     *
+     * @param string $action   Action name (should be unique)
+     * @param mixed  $callback Callback function as string
+     *                         or array with object reference and method name
+     */
+    abstract public function register_action(string $action, $callback);
+
+    /**
+     * Add buttons to the "attachmentmenu" container.
+     *
+     * @todo refactor this method into another helper class
+     *
+     * @param array[]     $btns the buttons
+     * @param null|string $skin the skin name
+     */
+    public function add_buttons_attachmentmenu(array $btns, ?string $skin = null): void
+    {
+        $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
+
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            $btn['_id'] = $btn['_id'] ?? 'WTF_NO_BASE_ID';
+            $btn['class'] = $btn['class'] ?? '';
+            $btn['classact'] = $btn['classact'] ?? '';
+            $btn['innerclass'] = $btn['innerclass'] ?? '';
+
+            $btn['type'] = 'link-menuitem';
+            $btn['id'] = "attachmenu{$btn['_id']}";
+
+            switch ($skin) {
+                case 'classic':
+                    $btn['class'] .= " {$btn['_id']}link";
+                    $btn['classact'] .= " {$btn['_id']}link active";
+                    $btn['innerclass'] .= " {$btn['_id']}link";
+                    break;
+                case 'elastic':
+                    $btn['class'] .= " {$btn['_id']} disabled";
+                    $btn['classact'] .= " {$btn['_id']} active";
+                    break;
+                case 'larry':
+                    $btn['class'] .= ' icon';
+                    $btn['classact'] .= ' icon active';
+                    $btn['innerclass'] .= " icon {$btn['_id']}";
+                    break;
+                default:
+                    break;
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'attachmentmenu');
+        }
+    }
+
+    /**
+     * Add buttons to the "loginfooter" container.
+     *
+     * @param array[]     $btns the buttons
+     * @param null|string $skin the skin name
+     */
+    public function add_buttons_loginfooter(array $btns, ?string $skin = null): void
+    {
+        $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
+
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            $btn['type'] = 'link';
+            $btn['class'] = $btn['class'] ?? '';
+            $btn['innerclass'] = $btn['innerclass'] ?? '';
+            $btn['badgeType'] = $btn['badgeType'] ?? 'secondary';
+
+            // should always has 'support-link' class
+            $btn['class'] .= ' support-link';
+
+            if ($skin === 'elastic') {
+                $btn['class'] .= " badge badge-{$btn['badgeType']}";
+                $btn['data-toggle'] = $btn['data-toggle'] ?? 'tooltip';
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'loginfooter');
+        }
+    }
+
+    /**
+     * Add buttons to the "mailboxoptions" container.
+     *
+     * @param array[]     $btns the buttons
+     * @param null|string $skin the skin name
+     */
+    public function add_buttons_mailboxoptions(array $btns, ?string $skin = null): void
+    {
+        $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'mailboxoptions');
+        }
+    }
+
+    /**
+     * Add buttons to the "taskbar" container.
+     *
+     * @param array[]     $btns the buttons
+     * @param null|string $skin the skin name
+     */
+    public function add_buttons_taskbar(array $btns, ?string $skin = null): void
+    {
+        $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
+
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            $btn['type'] = 'link';
+            $btn['class'] = $btn['class'] ?? '';
+            $btn['innerclass'] = $btn['innerclass'] ?? '';
+
+            switch ($skin) {
+                case 'classic':
+                    $btn['class'] .= ' button-nthu-ee';
+                    break;
+                case 'elastic':
+                    $btn['class'] .= ' nthu-ee manual';
+                    $btn['innerclass'] .= ' inner';
+                    break;
+                case 'larry':
+                    $btn['class'] .= ' button-nthu-ee';
+                    $btn['innerclass'] .= ' button-inner';
+                    break;
+                default:
+                    break;
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'taskbar');
+        }
+    }
+
+    /**
+     * Add buttons to the "toolbar" container.
+     *
+     * @param array[]     $btns the buttons
+     * @param null|string $skin the skin name
+     */
+    public function add_buttons_toolbar(array $btns, ?string $skin = null): void
+    {
+        $skin = $skin ?? RoundcubeHelper::getBaseSkinName();
+
+        $btns = \array_map(function (array $btn) use ($skin): array {
+            switch ($skin) {
+                case 'classic':
+                    $btn['class'] .= ' button';
+                    break;
+                case 'elastic':
+                    $btn['innerclass'] .= ' inner';
+                    break;
+                case 'larry':
+                    $btn['class'] .= ' button';
+                    break;
+                default:
+                    break;
+            }
+
+            return $btn;
+        }, $btns);
+
+        foreach ($btns as $btn) {
+            $this->add_button($btn, 'toolbar');
+        }
+    }
+
+    /**
+     * Load plugin configurations.
+     */
+    private function loadPluginConfigurations(): void
+    {
+        $configFiles = [
+            "{$this->home}/config.inc.php.dist",
+            "{$this->home}/config.inc.php",
+        ];
+
+        $rcmail = rcmail::get_instance();
+
+        $this->config = \array_reduce(
+            $configFiles,
+            function (array $carry, string $file): array {
+                \is_file($file) && (include $file);
+
+                return \array_merge($carry, (array) ($config ?? []));
+            },
+            []
+        );
+
+        $rcmail->config->merge([$this->ID => $this->config]);
+    }
+
+    /**
+     * Load user plugin preferences.
+     */
+    private function loadPluginPreferences(): void
+    {
+        $rcmail = rcmail::get_instance();
+
+        $prefsDefault = [
+            'enabled' => 1,
+            'viewer' => $this->config['viewer'],
+            'view_button_layouts' => $this->config['view_button_layouts'],
+        ];
+
+        $prefsUser = $rcmail->user->get_prefs()[$this->ID] ?? [];
+
+        $this->prefs = \array_merge($prefsDefault, $prefsUser);
+    }
+
+    /**
+     * Expose plugin configurations.
+     *
+     * @param null|array $keys the keys which will be exposed, null will expose all
+     */
+    private function exposePluginConfigurations(?array $keys = null): void
+    {
+        $rcmail = rcmail::get_instance();
+
+        if (null === $keys) {
+            $config = $this->config;
+        } else {
+            $config = [];
+            foreach ($keys as $key) {
+                $config[$key] = $this->config[$key];
+            }
+        }
+
+        $rcmail->output->set_env("{$this->ID}.config", $config);
+    }
+
+    /**
+     * Register plugin actions.
+     */
+    private function registerPluginActions(): void
+    {
+        $actions = (array) ($this->actions ?? []);
+
+        foreach ($actions as $action => $handler) {
+            $this->register_action("plugin.{$this->ID}.{$action}", [$this, $handler]);
+        }
+    }
+}
