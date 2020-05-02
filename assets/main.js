f8c61ef8cfb64884732d58e2ce68d41f54f04f7c
Jack Cherng <jfcherng@gmail.com>
2020-05-03 06:05:51 +0800

    style: use JS optional chaining
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,7 @@ const rcmail = global.rcmail;
 const rcube_parse_query = global.rcube_parse_query;
 
 let comm_path = rcube_parse_query(rcmail.env.comm_path);
-let is_framed = Boolean(parseInt(typeof comm_path._framed !== 'undefined' ? comm_path._framed : 0));
+let is_framed = Boolean(parseInt(comm_path?._framed || 0));
 
 if (!is_framed) {
   funcs.printNthuSloganToConsole();
